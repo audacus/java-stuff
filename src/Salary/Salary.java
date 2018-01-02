b3a169e884d3a523ea4fa746edb6509b5957ba17
audacus <david.burkhart@hotmail.com>
2018-01-02 02:36:36 +0100

    Eclipse project +eclipse project tree +Salary class

---
+++ src/Salary/Salary.java
@@ -0,0 +1,34 @@
+package Salary;
+
+public class Salary implements Comparable<Salary> {
+
+	enum Type {
+		Salary,
+		Fee
+	}
+
+	private int currentSalary;
+	private Type salaryType;
+
+	public Salary(int initial, Type type) {
+		this.currentSalary = initial;
+		this.salaryType = type;
+	}
+
+	public int getSalary() {
+		return this.currentSalary;
+	}
+
+	public String toString() {
+		return this.salaryType + ": " + this.currentSalary;
+	}
+
+	@Override
+	public int compareTo(Salary a) {
+		return this.currentSalary - a.getSalary();
+		
+		
+		// return -1, 0, 1
+		// return (int) Math.signum(this.currentSalary - a.getSalary());
+	}
+}
